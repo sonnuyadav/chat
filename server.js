f3072fe88e75dfda7f32d2455b118f5b3fb8746a
sonu yadav <sonu@pokermagnet>
2020-03-14 18:58:47 +0530

    second push

--- server.js
+++ server.js
@@ -20,5 +20,43 @@ mongo.connect('mongodb://127.0.0.1/chat',{ useNewUrlParser: true, useUnifiedTopo
       })
 
     //Get chats from mongo collection
+    chat.find().limit(100).sort({_id:1}).toArray(function(err,rest){
+        if(err){
+            throw err;
+        }
+        //Emit the message to the client
+        socket.emit('output', res);
+    });
+
+    //Handle input events
+    socket.on('input', function(data){
+        let name = data.name;
+        let message = data.message;
+
+        //check for name and message
+        if(name =='' || message ==''){
+            sendStatus('Please enter the name and message')
+
+        }else{
+            //Insert data in database
+            chat.insert({name:name, message:message}, function(){
+                client.emit('output', [data]);
+
+                //Send status object
+                sendStatus({
+                    message : "Message sent",
+                    clear: true
+                });
+            })
+        }
+    });
+    //handle clear
+    socket.on('clear', function(data){
+        //Remove all chat from the collection
+        chat.remove({}, function(){
+            //emit cleared
+            socket.emit('cleared')
+        })
+    });
 
 });
